78a1ef49d56035f3567dc40e3a1b6428c067fc93
Qays Ayad <work.worker55@gmail.com>
2020-05-30 02:17:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function updateDriverWithKeyAndValue(obj,key,value){
 return Object.assign({},obj,{[key]:value});
 }
 function destructivelyUpdateDriverWithKeyAndValue(obj,key,value){
-  Object.assign(obj,{[key]:value});
+return  Object.assign(obj,{[key]:value});
 
 }
 function deleteFromDriverByKey(obj,key,){
